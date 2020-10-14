37151b23076254319ec27142798d6c3934b9ec46
nariya <nariya@shota-2.local>
2020-10-14 17:02:46 +0900

    menuボタン完成

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import './App.css';
-import imaje from './headerimaje.jpeg';
-import aicon from './aicon-imaje.jpg'
+import image from './headerimage.jpeg';
+import icon from './icon-image.jpg';
+import Arrow from './Arrow.svg';
+
 function App() {
   return (
     <div class="main">
       <div class="main-header">
-        <span class="usearname">しょうた × エンジニア</span>
-        <span class="caunt">1,149件のツイート</span>
+        <div class="headerbackclick">
+          <img src={Arrow} alt="icon" class="backclick" />
+        </div>
+        <div class="usearnamecaunt">
+          <span class="usearname">しょうた × エンジニア</span>
+          <span class="caunt">1,149件のツイート</span>
+        </div>
       </div>
 
-      <img className="headerimaje" src={imaje} alt="headerimaje" />
+      <img className="headerimage" src={image} alt="headerimage" />
       <div class="main-visual">
+        <div class="main-messege">
+          <div class="actionbutton">
+            <img className="icon" src={icon} alt="icon" />
+            <div class="menubutton">
+              <div class="menubutton1">
+                <div class="menubuttonmini"></div>
+                <div class="menubuttonmini"></div>
+                <div class="menubuttonmini"></div>
+              </div>
+            </div>
+          </div>
+          <span class="iconname">しょうた × エンジニア</span>
+          <span class="id">@shota1995m</span>
+        </div>
       </div>
-      <img className="aicon" src={aicon} alt="aicon" />
-
-      <div class="main-messege">
-        <span class="usearname">しょうた × エンジニア</span>
-        <span class="id">@shota1995m</span>
+      <div class="underimage">
+        <span class="undercoment">ブロックされています。</span>
+        <span class="id">@shota1995mさんのフォローやツイートの表示はできません。</span>
+        <a href="" className="link"><span>詳細はこちら</span></a>
       </div>
-
-    </div>
+    </div >
   );
 }
 
